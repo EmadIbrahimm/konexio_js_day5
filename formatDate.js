71aea7a540dae0b7599d33edca4bddbad8eda85a
EmadIbrahimm <65893940+EmadIbrahimm@users.noreply.github.com>
2020-06-18 17:39:25 +0200

    Add files via upload

---
+++ formatDate.js
@@ -0,0 +1,16 @@
+// var now = new Date
+// console.log(now);
+
+// function dateFormat(str);{
+//     var format = '2018-11-17'
+//     var newFormat = '17/11/2018'
+
+// }
+
+// console.log(dateFormat.length(gtFullDate))
+
+
+var oldF = '2018-11-17'
+var newF = '17/11/2018'
+const currantF = oldF 
+oldF = newF 
